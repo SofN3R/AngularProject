ec9f0173b5db331ac90a3df677e8aacf551a5531
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2022-03-02 11:33:40 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+// Angular Imports
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// My imports
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page404Component } from './components/page404/page404.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    LoginComponent,
+    HomeComponent,
+    SliderComponent,
+
+
   ],
   imports: [
     BrowserModule,
